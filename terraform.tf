576bd7c8fb2b4044cd135591fc902c8b35898265
Gurret Lionel <73423955+gurretl@users.noreply.github.com>
2024-10-23 07:56:42 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "> 3.71, <= 3.99.0"
+      version = "3.116.0"
+      // version = "> 3.71, <= 3.99.0"
     }
     modtm = {
       source  = "Azure/modtm"
